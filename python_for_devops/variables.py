21183bb9ac28da422dac2b709c41f25195da9ce4
aftab <aftab.shaikh@ecosmob.com>
2025-10-23 18:19:11 +0530

    variables

--- python_for_devops/variables.py
+++ python_for_devops/variables.py
@@ -4,4 +4,28 @@ b = 42
 
 ans = b + 8
 
-print(f'{a}, {b} sum of ans is {ans}')
+is_active = False
+
+list_1 = [1,2,3,4,5,6]
+
+tuple_1 = (1,2,3,4,5,6)
+
+dic_1 = {'name':'aftab'}
+
+list_2 = [{
+    'name':'aftab',
+    'age': 22
+    },
+    {
+        'name': 'second_aftab',
+        'name': 23
+    }
+]
+
+
+dic_2 = {
+    'name' :['aftab', 'second_aftab'],
+    'age': [22,23]
+}
+
+print(f'{a}, {b} sum of ans is {ans} is user active {is_active}')
