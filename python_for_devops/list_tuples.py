cbef73d8ceb51c0351a37d91ea2a37ddd1a8752e
aftab <aftab.shaikh@ecosmob.com>
2025-10-30 13:24:04 +0530

    list and tuples

--- python_for_devops/list_tuples.py
+++ python_for_devops/list_tuples.py
@@ -9,3 +9,7 @@ s3_buckets.append(grey_bucket);
 print(s3_buckets)
 
 ### So with this example we can use list and tuples in real life scenrios for s3 we can create and delete the components but there should be only defined number of admins in the orgs so the values that are not need to be manipulated frequently they should be used in a tuple. 
+
+s3_buckets.remove(grey_bucket)
+
+print(s3_buckets)
